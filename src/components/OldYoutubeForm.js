ffac98d34e995905be0b56574b52da5831d2cda8
HumayunHabib <humayun.habib@magnusmage.com>
2022-08-22 10:17:26 +0500

    fromik form

--- src/components/OldYoutubeForm.js
+++ src/components/OldYoutubeForm.js
@@ -14,11 +14,9 @@ const onSubmit = (values) => {
 
 // const validate = values => {
 //   const errors = {}
-
 //   if (!values.name) {
 //     errors.name = 'Required'
 //   }
-
 //   if (!values.email) {
 //     errors.email = 'Required'
 //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
